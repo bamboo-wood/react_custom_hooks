25b30de990b474fa876f34051ee36129230ab384
bamboo-wood <cantate.1009.0417@gmail.com>
2023-04-05 01:37:29 +0900

    fetch data no cosutom hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,53 @@
-import React from "react";
+import { useState } from "react";
 import { UserCard } from "./components/UserCard";
-
-const user = {
-  id: 1,
-  name: "John Doe",
-  email: "example@gmail.com",
-  address: "123 Main St, Anytown, USA",
-};
+import axios from "axios";
+import { User } from "./types/api/user";
+import { UserProfile } from "./types/userProfile";
 
 function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+
+  const [loading, setLoading] = useState(false);
+
+  const [error, setError] = useState(false);
+
+  const onClickFetchUser = () => {
+    setLoading(true);
+    setError(false);
+
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => {
+          return {
+            id: user.id,
+            name: `${user.name} (${user.username})`,
+            email: user.email,
+            address: `${user.address.city}, ${user.address.street}, ${user.address.suite}`,
+          };
+        });
+        setUserProfiles(data);
+      })
+      .catch((error) => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
     <div className="App">
-      <UserCard user={user} />
+      <button onClick={onClickFetchUser}>fetch data</button>
+      <br />
+
+      {error ? (
+        <p>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>loading...</p>
+      ) : (
+        userProfiles.map((user) => <UserCard key={user.id} user={user} />)
+      )}
     </div>
   );
 }
