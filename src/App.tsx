97a2b8c060c4c8b50e288c01f448646d0650ac6e
bamboo-wood <cantate.1009.0417@gmail.com>
2023-04-05 01:56:49 +0900

    add custom hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,12 @@
-import { useState } from "react";
 import { UserCard } from "./components/UserCard";
-import axios from "axios";
-import { User } from "./types/api/user";
-import { UserProfile } from "./types/userProfile";
+import { useAllUsers } from "./hooks/useAllUsers";
 
 function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-
-  const [loading, setLoading] = useState(false);
-
-  const [error, setError] = useState(false);
-
-  const onClickFetchUser = () => {
-    setLoading(true);
-    setError(false);
-
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => {
-          return {
-            id: user.id,
-            name: `${user.name} (${user.username})`,
-            email: user.email,
-            address: `${user.address.city}, ${user.address.street}, ${user.address.suite}`,
-          };
-        });
-        setUserProfiles(data);
-      })
-      .catch((error) => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
 
   return (
     <div className="App">
-      <button onClick={onClickFetchUser}>fetch data</button>
+      <button onClick={getUsers}>fetch data</button>
       <br />
 
       {error ? (
